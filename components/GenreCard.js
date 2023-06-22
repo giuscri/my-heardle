fc75c662d81d8dce94e4fd42a2c5b86420e0f2dd
Giuseppe Crinò <giuscri@gmail.com>
2023-06-22 12:26:11 +0200

    feat: add summer playlist info and indices on top of artwork

--- components/GenreCard.js
+++ components/GenreCard.js
@@ -1,10 +1,13 @@
-export default function GenreCard({ genreId, genreHuman, artworkSrc, url, title, artist, thumbnail }) {
+export default function GenreCard({ i, genreId, genreHuman, artworkSrc, url, title, artist, thumbnail }) {
     return (
         <a href={`/play?audio-src=${encodeURIComponent(url)}&title=${encodeURIComponent(title)}&artist=${encodeURIComponent(artist)}&thumbnail-src=${encodeURIComponent(thumbnail)}`}>
-        <div className="text-center flex flex-col text-sm">
-            <p className="text-lime-50" style={{textShadow: 'yellow 1px 0 10px'}}>{genreHuman}</p>
-            <img src={artworkSrc} className="h-[250px] self-center items-center rounded-sm shadow-xl hover:shadow-lime-50 hover:scale-110 focus:scale-110 transition-all duration-500 delay-75" />
-        </div>
+            <div className="text-center text-sm">
+                <p className="text-lime-50" style={{textShadow: 'yellow 1px 0 10px'}}>{genreHuman}</p>
+                <div className="relative flex justify-center items-center transition-all duration-500 delay-75 hover:shadow-lime-50 hover:scale-110 focus:scale-110">
+                    <p className="absolute text-4xl font-bold text-white z-10">{i}</p>
+                    <img src={artworkSrc} className="h-[250px] rounded-sm shadow-xl" />
+                </div>
+            </div>
         </a>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,90 @@
 import GenreCard from "../components/GenreCard";
 import { createClient } from 'redis';
 
-export default function Genre({ pop, metal, evergreen, cartoons, christmas, italians }) {
+export default function Genre({ pop, metal, evergreen, cartoons, /*christmas,*/ summer, italians }) {
+    const genres = [
+        {
+            genreHuman: "Evergreen",
+            genreId: "evergreen",
+            artworkSrc: '/nina-simone.jpg',
+            url: evergreen.url,
+            title: evergreen.title,
+            artist: evergreen.artist,
+            thumbnail: evergreen.thumbnail
+        },
+        {
+            genreHuman: "Pop",
+            genreId: "pop",
+            artworkSrc: '/anastacia.jpg',
+            url: pop.url,
+            title: pop.title,
+            artist: pop.artist,
+            thumbnail: pop.thumbnail
+        },
+        {
+            genreHuman: "Metal & Rock",
+            genreId: "metal",
+            artworkSrc: '/megadeth.jpg',
+            url: metal.url,
+            title: metal.title,
+            artist: metal.artist,
+            thumbnail: metal.thumbnail
+        },
+        {
+            genreHuman: "Cartoons",
+            genreId: "cartoons",
+            artworkSrc: '/goku.gif',
+            url: cartoons.url,
+            title: cartoons.title,
+            artist: cartoons.artist,
+            thumbnail: cartoons.thumbnail
+        },
+        // uncomment this when you need it
+        // {
+        //     genreHuman: "Christmas",
+        //     genreId: "christmas",
+        //     artworkSrc: '/george-michael.jpg',
+        //     url: christmas.url,
+        //     title: christmas.title,
+        //     artist: christmas.artist,
+        //     thumbnail: christmas.thumbnail
+        // },
+        {
+            genreHuman: "Festivalbar",
+            genreId: "summer",
+            artworkSrc: '/festivalbar.jpg',
+            url: summer.url,
+            title: summer.title,
+            artist: summer.artist,
+            thumbnail: summer.thumbnail
+        },
+        {
+            genreHuman: "Italians",
+            genreId: "italians",
+            artworkSrc: '/883.jpg',
+            url: italians.url,
+            title: italians.title,
+            artist: italians.artist,
+            thumbnail: italians.thumbnail
+        }
+    ]
+
     return (
-    <div className="grid grid-cols-3 items-center">
-        <GenreCard
-        genreHuman="Evergreen"
-        genreId="evergreen"
-        artworkSrc='/nina-simone.jpg'
-        url={evergreen.url}
-        title={evergreen.title}
-        artist={evergreen.artist}
-        thumbnail={evergreen.thumbnail}
-        />
-
-        <GenreCard
-        genreId="pop"
-        genreHuman="Pop"
-        artworkSrc='/anastacia.jpg'
-        url={pop.url}
-        title={pop.title}
-        artist={pop.artist}
-        thumbnail={pop.thumbnail}
-        />
-
-        <GenreCard
-        genreId="metal"
-        genreHuman="Metal & Rock"
-        artworkSrc='/megadeth.jpg'
-        url={metal.url}
-        title={metal.title}
-        artist={metal.artist}
-        thumbnail={metal.thumbnail}
-        />
-
-        <GenreCard
-        genreHuman="Cartoons"
-        genreId="cartoons"
-        artworkSrc='/goku.gif'
-        url={cartoons.url}
-        title={cartoons.title}
-        artist={cartoons.artist}
-        thumbnail={cartoons.thumbnail}
-        />
-
-        <GenreCard
-        genreHuman="Christmas"
-        genreId="christmas"
-        artworkSrc='/george-michael.jpg'
-        url={christmas.url}
-        title={christmas.title}
-        artist={christmas.artist}
-        thumbnail={christmas.thumbnail}
-        />
-
-        {/* <GenreCard
-        genreHuman="Summer"
-        genreId="summer"
-        artworkSrc='/borat.jpg'
-        url={summer.url}
-        title={summer.title}
-        artist={summer.artist}
-        thumbnail={summer.thumbnail}
-        /> */}
-
-        <GenreCard
-        genreHuman="Italians"
-        genreId="italians"
-        artworkSrc='/883.jpg'
-        url={italians.url}
-        title={italians.title}
-        artist={italians.artist}
-        thumbnailSrc={italians.thumbnailSrc}
-        />
-    </div>
+        <div className="grid grid-cols-3 items-center">
+            {genres.map((genre, index) => 
+                <GenreCard
+                    i={index+1}
+                    key={genre.genreId}
+                    genreHuman={genre.genreHuman}
+                    genreId={genre.genreId}
+                    artworkSrc={genre.artworkSrc}
+                    url={genre.url}
+                    title={genre.title}
+                    artist={genre.artist}
+                    thumbnail={genre.thumbnail}
+                />
+            )}
+        </div>
     )
 }
 
@@ -81,12 +92,19 @@ export async function getServerSideProps() {
     const client = createClient({ url: 'redis://localhost:6379' })
     await client.connect()
 
-    const genres = [ 'pop', 'metal', 'christmas', 'cartoons', 'italians', 'evergreen' ]
+    // const genres = [ 'pop', 'metal', 'christmas', 'cartoons', 'italians', 'evergreen' ]
+    const genres = [ 'pop', 'metal', 'summer', 'cartoons', 'italians', 'evergreen' ]
 
     let props = {}
     for (let g of genres) {
-        props[g] = JSON.parse(await client.LPOP(g))
-        await client.RPUSH(g, JSON.stringify(props[g])) // prevent songs from finishing while playing
+        // props[g] = {} // 
+        const j = await client.LPOP(g)
+        try {
+            props[g] = JSON.parse(j) || {} // Allow genre Redis list to be empty without breaking the app
+            await client.RPUSH(g, JSON.stringify(props[g])) // Prevent songs from finishing while having fun
+        } catch (e) {
+            console.log(e, j)
+        }
     }
 
     return { props }
